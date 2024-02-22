aff4aaf9ac48414a9e275e4a42afd438faf8a9a4
DanielaPSousa <danielasousa.info@gmail.com>
2024-02-22 21:16:36 +0000

    Add comments to the code

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+/// <summary>
+/// The beginning of the class program
+/// </summary>
     class Program
     {
+    /// <summary>
+    /// The program arguments
+    /// </summary>
+    /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
